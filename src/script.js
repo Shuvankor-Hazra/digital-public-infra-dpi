468e73dd1d0420f58c6bf62a04ec3068586d83f7
Shuvankr Hazra <shuvo.s.ovuhs@gmail.com>
2024-11-09 18:05:36 +0600

    Footer new added

--- src/script.js
+++ src/script.js
@@ -43,3 +43,27 @@ document.addEventListener("click", (event) => {
     mobileMenu.style.display = "none";
   }
 });
+
+
+
+
+
+// Blog 
+      document.addEventListener('DOMContentLoaded', function() {
+        const swiper = new Swiper('.swiper', {
+          direction: 'horizontal',
+            loop: true,
+          slidesPerView: 3,
+          pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+          },
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+          scrollbar: {
+            el: '.swiper-scrollbar',
+          },
+        });
+      });
