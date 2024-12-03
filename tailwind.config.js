0eccf5196252626352445ebe9b630b435e0fb816
Shuvankor Hazra <117534734+Shuvankor-Hazra@users.noreply.github.com>
2024-12-03 22:50:00 +0600

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: { 
-  "custom-gradient": `linear-gradient(158deg, rgba(0, 154, 132, 0) 42%, #f3c60c),  
+  "custom-gradient": `linear-gradient(158deg, rgba(0, 154, 132, 0) 42%, #D3222A),  
                       linear-gradient(184deg, rgba(0, 154, 132, 0.93), #009a84 59%),  
                       linear-gradient(187deg, #009a84, rgba(0, 154, 132, 0) 0%, rgba(0, 154, 132, 0.29) 0%, #006f63)`, 
 },
